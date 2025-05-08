6950b8b689bf9d1aa38958f0efd6dc9e49de22cf
Ethan-bro <ethan.brook@mail.utoronto.ca>
2025-05-08 17:19:16 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         FizzBuzz fizzBuzz = new FizzBuzz();
         FizzBuzz.performFizzBuzz(5);
         FizzBuzz.main(args);
+        // This is a comment made in GitHub and if you see this in Intelij, then "git pull" worked!!!
     }
 }
