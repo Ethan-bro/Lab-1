8ef2b515f292dd63155ae61b4184505be19291da
Ethan-bro <ethan.brook@mail.utoronto.ca>
2025-05-08 17:17:02 -0400

    Added a Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        FizzBuzz.performFizzBuzz(5);
+        FizzBuzz.main(args);
+    }
+}
